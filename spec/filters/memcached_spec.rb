45e4ed1588f2abc3136e793fe87d5b7925601e80
João Duarte <jsvd@users.noreply.github.com>
2018-11-22 23:34:31 +0000

    Use docker for testing and convert integration tests to unit tests (#2)

--- spec/filters/memcached_spec.rb
+++ spec/filters/memcached_spec.rb
@@ -5,28 +5,49 @@ require "logstash/filters/memcached"
 LogStash::Logging::Logger::configure_logging("TRACE")
 
 describe LogStash::Filters::Memcached do
-  describe "Set to Hello World" do
-    let(:config) do <<-CONFIG
-      filter {
-        memcached {
-          hosts => ["localhost:11211"]
-          set => {
-            "[answer]" => "success/%{success}/answer"
-          }
-          get => {
-            "success/%{success}/answer" => "[ultimate][answer]"
-          }
-        }
+  subject { described_class.new(config) }
+  let(:cache) { double('memcached') }
+  before(:each) do
+    allow(subject).to receive(:establish_connection).and_return(cache)
+    allow(subject).to receive(:close)
+    subject.register
+  end
+
+  after(:each) do
+    subject.close
+  end
+
+  describe "#get" do
+    let(:event) { ::LogStash::Event.new(data) }
+    let(:config) do
+      {
+        "hosts" => ["localhost:11211"],
+        "get" => { "success/%{success}/answer" => "[ultimate][answer]" }
       }
-    CONFIG
     end
+    let(:data) { { "success" => "true" } }
 
-    sample("answer" => "42", "success" => "true") do
-      expect(subject).to include("answer")
-      expect(subject.get('answer')).to eq("42")
+    it "retrieves data from memcache" do
+      expect(cache).to receive(:get_multi).with(["success/true/answer"]).and_return({"success/true/answer" => "42"})
+      subject.filter(event)
+      expect(event.get("ultimate")).to eq("answer" => "42")
+    end
+  end
 
-      expect(subject).to include("ultimate")
-      expect(subject.get("ultimate")).to eq("answer" => "42")
+  describe "#set" do
+    let(:event) { ::LogStash::Event.new(data) }
+    let(:config) do
+      {
+        "hosts" => ["localhost:11211"],
+        "set" => { "[answer]" => "success/%{success}/answer" },
+      }
+    end
+    let(:data) { { "answer" => "42", "success" => "true" } }
+    before(:each) { allow(cache).to receive(:multi) {|&b| b.call } }
+
+    it "sets data on memcache" do
+      expect(cache).to receive(:set).with("success/true/answer", "42")
+      subject.filter(event)
     end
   end
 end
